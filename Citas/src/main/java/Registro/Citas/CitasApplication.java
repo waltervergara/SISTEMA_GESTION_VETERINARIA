c7b46e54e3a5c90f8bf362faae01a59507394aa8
Alonso_Contreras <alon.contreras@duocuc.cl>
2026-06-23 11:09:26 -0400

    Implementacion de YAML y Eureka

--- Citas/src/main/java/Registro/Citas/CitasApplication.java
+++ Citas/src/main/java/Registro/Citas/CitasApplication.java
@@ -2,8 +2,10 @@ package Registro.Citas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients//la antena que permite comunarse con otros servicios creados
 public class CitasApplication {
--- Citas/src/main/java/Registro/Citas/Client/EmpleadoClient.java
+++ Citas/src/main/java/Registro/Citas/Client/EmpleadoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "microservicio-registro-empleado" , url = "http://localhost:8081")
+@FeignClient(name = "empleados")
 public interface EmpleadoClient {
 
     @GetMapping("/api/v1/registro/empleados/buscar/{runEmpleado}")
