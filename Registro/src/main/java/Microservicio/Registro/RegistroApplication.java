ece8b53b13127a69ff8d10256cb3fc8f6fbad10f
Alonso_Contreras <alon.contreras@duocuc.cl>
2026-06-23 11:06:28 -0400

    Implementamos el formato YAML en el propierties

--- Registro/src/main/java/Microservicio/Registro/RegistroApplication.java
+++ Registro/src/main/java/Microservicio/Registro/RegistroApplication.java
@@ -1,8 +1,10 @@
 package Microservicio.Registro;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RegistroApplication {
 
