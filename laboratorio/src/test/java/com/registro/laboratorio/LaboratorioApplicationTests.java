d2e4406aba18870b6c898e8e0aec3c0bbc0fdb89
Alonso_Contreras <alon.contreras@duocuc.cl>
2026-06-28 19:52:18 -0400

    Implementacion de documentacio OpenApi , docker , formato yml

---
+++ laboratorio/src/test/java/com/registro/laboratorio/LaboratorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registro.laboratorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboratorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
